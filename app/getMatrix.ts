b3904379fa17525f7dab81e628eac847ca73cd6b
Adonay Amar <adonayamar@gmail.com>
2025-06-09 23:52:30 -0400

    Create getMatrix

---
+++ app/getMatrix.ts
@@ -0,0 +1,29 @@
+const matrix: number[][] = [
+  [1, 2, 3, 4, 5, 6, 7, 8, 9],
+  [1, 2, 3, 4, 5, 6, 7, 8, 9],
+  [1, 2, 3, 4, 5, 6, 7, 8, 9],
+  [1, 2, 3, 4, 5, 6, 7, 8, 9],
+  [1, 2, 3, 4, 5, 6, 7, 8, 9],
+  [1, 2, 3, 4, 5, 6, 7, 8, 9],
+  [1, 2, 3, 4, 5, 6, 7, 8, 9],
+  [1, 2, 3, 4, 5, 6, 7, 8, 9],
+  [1, 2, 3, 4, 5, 6, 7, 8, 9],
+];
+
+const shuffle = (array: number[]) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+};
+
+function getMatrix() {
+  for (let i = 0; i < matrix.length; i++) {
+    matrix[i] = matrix[i] = shuffle(matrix[i]);
+  }
+
+  return matrix;
+}
+
+export default getMatrix;
