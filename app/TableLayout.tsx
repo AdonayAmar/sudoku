3ef24c62304eb73c65b28b0ce1c61fb1bb6f2a8f
Adonay Amar <adonayamar@gmail.com>
2025-06-09 23:53:52 -0400

    Refactor: TableLayout changed to Grid

--- app/TableLayout.tsx
+++ app/TableLayout.tsx
@@ -1,40 +1,37 @@
 "use client";
-import { Box, Table, Flex } from "@radix-ui/themes";
-import React, { useState } from "react";
+import { Grid } from "@radix-ui/themes";
+import getMatrix from "./getMatrix";
+import { useState } from "react";
+
+const matrix = getMatrix();
 
 const TableLayout = () => {
   const [activeCell, setActiveCell] = useState("");
   console.log(activeCell);
 
-  const cellId = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
   return (
-    <Box className="p-10">
-      <Table.Root>
-        <Table.Body>
-          {cellId.map((row) => (
-            <>
-              <Flex>
-                <Table.Row>
-                  {cellId.map((col) => (
-                    <Table.Cell
-                      key={`${row}${col}`}
-                      className="border w-10 p-0"
-                      style={
-                        activeCell === `${row}${col}`
-                          ? { backgroundColor: "green" }
-                          : { backgroundColor: "white" }
-                      }
-                      onClick={() => setActiveCell(`${row}${col}`)}
-                    ></Table.Cell>
-                  ))}
-                </Table.Row>
-              </Flex>
-            </>
-          ))}
-        </Table.Body>
-      </Table.Root>
-    </Box>
+    <Grid columns="3" className="border-3 w-150 h-150">
+      {matrix.map((block, blockId) => (
+        <>
+          <Grid columns="3" rows="3" className="border-3">
+            {block.map((item, id) => (
+              <Grid
+                key={`${blockId} ${id}`}
+                className="border place-items-center content-center"
+                style={
+                  activeCell === `${blockId} ${id}`
+                    ? { backgroundColor: "green" }
+                    : { backgroundColor: "white" }
+                }
+                onClick={() => setActiveCell(`${blockId} ${id}`)}
+              >
+                {item}
+              </Grid>
+            ))}
+          </Grid>
+        </>
+      ))}
+    </Grid>
   );
 };
 
